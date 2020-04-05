8cf42459a305622c08f79e052e8cc09e2d8240a8
droll-window-9768 <ppirotte@gmail.com>
2020-04-05 19:37:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ function subtract(a,b) {
 }
 
 function multiple(a,b) {
-  return (a*b)
+  return (a * b)
+}
+
+function multiply(a,b) {
+  return (a * b)
 }
 
 function divide(a,b) {
-  return (a/b)
+  return (a / b)
 }
+
